3bee524684e10eafe8d7e832b4e0d716dc44cbda
LikeRudin <likeRudin@gmail.com>
2023-06-25 17:23:28 +0900

    redux exercise by vanila-js redux
    (learn about how to set up redux
    and what is the roll of core-method of store)

--- src/index.js
+++ src/index.js
@@ -1,12 +1,44 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import {legacy_createStore} from 'redux';
+
+const plusBtn = document.querySelector('#plus');
+const minusBtn = document.querySelector('#minus');
+const numSpan = document.querySelector('#num');
+
+const PLUS ="plus";
+const MINUS ="minus";
+
+const reducer = (num = 0, action )=>{
+    console.log("reducer is running");
+    switch(action.type){
+        case PLUS:
+            return ++num;
+        case MINUS:
+            return --num;
+        default: 
+            return num;
+    }
+};
 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const numStore = legacy_createStore(reducer);
 
+const handleChangeNum = () =>{
+    console.log("num changed");
+    numSpan.innerHTML = numStore.getState();
+};
+
+numStore.subscribe(handleChangeNum);
+
+
+const handlePlusBtnClick = () =>{
+    console.log("dispatch plus");
+    numStore.dispatch({type: PLUS});
+};
+
+const handleMinusBtnClick = () =>{
+    console.log("dispatch minus");
+    numStore.dispatch({type: MINUS});
+};
+
+plusBtn.addEventListener('click', handlePlusBtnClick);
+minusBtn.addEventListener('click', handleMinusBtnClick);
